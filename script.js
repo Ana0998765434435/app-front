3da7cbe8d799403542f719c3dd6223487c3fbf9a
Ana0998765434435 <ana.j.maciel7@aluno.senai.br>
2026-08-13 12:08:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ const b = document.getElementById("btn")
 b.addEventListener("click", () => {
     console.log(nome.value)
 })
+
+async function criarUsuario (){
+    const novoUsuario = { nome: 'Ana', idade: 25};
+
+    const resposta = await fetch ('https://exemplo', {
+      method:'POST',
+      headers:}
+        'Content-Type': 'applicatcation/json'
+    )},
+     body: JSON.stringitfy(novoUsuario) {
+    };
+    const resultado = await reposta.json
+    console.log('Criado com sucesso:', resultado);
